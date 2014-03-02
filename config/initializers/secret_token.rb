c76585355b256f332b33c9347c6f2b37120cc830
Viktor Zavala <viktor@acklenavenue.com>
2014-03-02 01:27:18 -0600

    initial app, rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ControlProCaps::Application.config.secret_key_base = '36a1a882f22b47cd3044c2949de8aac7773ab6efbe96929d8d97a0fd1f64a0eb92d6c47afd4a3dd4941aee98852c26be5f7c6bd11b8daee9297cb574f726092e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ControlProCaps::Application.config.session_store :cookie_store, key: '_ControlProCaps_session'
